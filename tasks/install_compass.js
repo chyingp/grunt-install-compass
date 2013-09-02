10f1af499916bbf756594cb0ceffcb2163552a73
chyingp <chyingp@gmail.com>
2013-09-02 18:39:23 +0800

    bug fix: doesn't work in window 7

--- tasks/install_compass.js
+++ tasks/install_compass.js
@@ -10,34 +10,54 @@
 
 module.exports = function(grunt) {
 
-	var path = require('path');
+	var path = require('path'),
+		os = require('os'),
+		exec = require('child_process').exec,
+		child;
 
 	grunt.registerMultiTask('install_compass', 'Your task description goes here.', function() {
 
-		console.log('grunt-install-compass 开始!');
+		console.log('grunt-install-compass started!');
 
-		console.log(process.cwd());
-		var path = require('path');
-		var spawn = require('child_process').spawn,
-			ls = spawn('sudo', ['gem', 'install', '--local', 'compass-0.12.2.gem'], {
-				cwd: path.resolve( process.cwd(), 'node_modules/grunt-install-compass/tasks/compass' ),
-				stdio: ['pipe', 'pipe', 'pipe']
+		var command_line = 'gem install --local compass-0.12.2.gem';
+		if(os.type()!=='Windows_NT'){
+			console.log('os: not Windows_NT!');
+			command_line = 'sudo '+command_line;
+		}else{
+			console.log('os: Windows_NT!');
+		}
+		console.log('command_line: '+command_line);
+		child = exec(command_line, {
+				cwd: 'node_modules/grunt-install-compass/tasks/compass'
+			},
+			function (error, stdout, stderr) {
+				console.log('stdout: ' + stdout);
+				console.log('stderr: ' + stderr);
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
 			});
 
-		console.log('current path: '+process.cwd());
-
-		ls.stdout.on('data', function (data) {
-			console.log('stdout: ' + data);
-		});
-
-		ls.stderr.on('data', function (data) {
-			console.log('stderr: ' + data);
-		});
-
-		ls.on('close', function (code) {
-			console.log('child process exited with code ' + code);
-		});
-		console.log('grunt-install-compass 结束!');
+//		var spawn = require('child_process').spawn,
+//			ls = spawn('sudo', ['gem', 'install', '--local', 'compass-0.12.2.gem'], {
+//				cwd: path.resolve( process.cwd(), 'node_modules/grunt-install-compass/tasks/compass' ),
+//				stdio: ['pipe', 'pipe', 'pipe']
+//			});
+//
+//		console.log('current path: '+process.cwd());
+//
+//		ls.stdout.on('data', function (data) {
+//			console.log('stdout: ' + data);
+//		});
+//
+//		ls.stderr.on('data', function (data) {
+//			console.log('stderr: ' + data);
+//		});
+//
+//		ls.on('close', function (code) {
+//			console.log('child process exited with code ' + code);
+//		});
+		console.log('grunt-install-compass ended!');
 
 	});
 
